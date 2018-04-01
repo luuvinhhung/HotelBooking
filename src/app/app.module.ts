396cbc6c4fff07bd2e5fadbc5d1ef82113d678d5
LuuVinhHung <luuvinhhung159@gmail.com>
2018-04-01 10:22:22 +0700

    01-04-2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
-import { FilterComponent } from './filter/filter.component';
 import { FirstpageComponent } from './firstpage/firstpage.component';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
 import { MaterialModule} from './material.module';
-import { HotelsComponent } from './main/hotels/hotels.component';
 import { FormsModule } from '@angular/forms';
-import { MainComponent } from './main/main.component';
-import { HomesearchComponent } from './homesearch/homesearch.component';
+
+import { AppComponent } from './app.component';
+import { HotelsComponent } from './home/main/hotel/hotel.component';
+import { MainComponent } from './home/main/main.component';
+import { HomesearchComponent } from './home/homesearch/homesearch.component';
 import { HomeComponent } from './home/home.component';
+import { FilterComponent } from './home/filter/filter.component';
 
 @NgModule({
   declarations: [
--- src/app/home/main/hotel/hotel.component.ts
+++ src/app/home/main/hotel/hotel.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hotels',
-  templateUrl: './hotels.component.html',
-  styleUrls: ['./hotels.component.css']
+  templateUrl: './hotel.component.html',
+  styleUrls: ['./hotel.component.css']
 })
 export class HotelsComponent implements OnInit {
   hotelPoster: String = 'assets/category1.png';
-  constructor() { }
+  overviewtext: String = '';
+  constructor() {
+    this.overviewtext = 'Đà Nẵng, 8,3 km tới Trung tâm thành phố';
+   }
   ngOnInit() {
   }
 
